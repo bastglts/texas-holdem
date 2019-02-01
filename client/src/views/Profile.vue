7aec838d4f54adc0bdbad1fb4f541379834b74fe
Bastien Gili-tos <bast.gilitos@gmail.com>
2019-02-01 17:47:55 +0100

    Add dummy profile

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="profile">
-    TODO User Profile
+    <div>Username: {{user.username}} </div>
+    <div>Money: ${{user.count}} </div>
   </div>
 </template>
 
 <script>
+import auth from '../utils/auth';
+
+
 export default {
   name: 'Profile',
+
+  data() {
+    return {
+      user: {},
+    };
+  },
+
+  methods: {
+    fetchUserData() {
+      auth.fetchUserData().then((data) => {
+        this.user = data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchUserData();
+  },
 };
 </script>
