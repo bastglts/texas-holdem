65dd66319621df8646a499ff02b54c040f1c27da
Bastien Gili-tos <bast.gilitos@gmail.com>
2019-02-01 17:04:11 +0100

    Comment out 'create table functionality'

--- client/src/views/Tables.vue
+++ client/src/views/Tables.vue
@@ -3,14 +3,14 @@
     <h3>Tables list:</h3>
     <p>There's just one table for now. <router-link to="/table">Enter here.</router-link> </p>
 
-    <h3>Create new table ?</h3>
+<!--     <h3>Create new table ?</h3>
     <form>
       <input type="text"
             v-model="tableName"
             placeholder="table name"
             @keyup.enter="createTable">
       <button @click="createTable">Create</button>
-    </form>
+    </form> -->
   </div>
 
 </template>
