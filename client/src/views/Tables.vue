caec86c9db80c682344ab3a2dce32ce1e64ae9dc
Bastien Gili-tos <bast.gilitos@gmail.com>
2019-02-07 11:38:40 +0100

    Add create table form with veeValidate

--- client/src/views/Tables.vue
+++ client/src/views/Tables.vue
@@ -3,21 +3,29 @@
     <h3>Tables list:</h3>
     <p>There's just one table for now. <router-link to="/table">Enter here.</router-link> </p>
 
-<!--     <h3>Create new table ?</h3>
-    <form>
+    <h3>Create new table ?</h3>
+    <form @submit.prevent="validateBeforeCreate">
       <input type="text"
-            v-model="tableName"
-            placeholder="table name"
-            @keyup.enter="createTable">
-      <button @click="createTable">Create</button>
-    </form> -->
+             v-model="tableName"
+             name="tableName"
+             v-validate="'required|unique'"
+             data-vv-delay="600"
+             placeholder="table name">
+      <span v-show="errors.has('tableName')" class="warning">{{ errors.first('tableName') }}</span>
+      <button type="submit">Create</button>
+    </form>
   </div>
 
 </template>
 
 <script>
+import isUnique from '../services/isUnique';
+import TableService from '../services/TableService';
+
+
 export default {
   name: 'Tables',
+
 
   data() {
     return {
@@ -25,11 +33,25 @@ export default {
     };
   },
 
+
   methods: {
+    validateBeforeCreate() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.createTable();
+        }
+      });
+    },
+
     createTable() {
-      console.log('create table');
+      TableService.createTable(this.tableName)
+        .then(res => console.log(res));
     },
   },
-};
 
+
+  mounted() {
+    isUnique('table');
+  },
+};
 </script>
