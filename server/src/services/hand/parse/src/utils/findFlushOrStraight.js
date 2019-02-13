bde1efd19100afc9302faea58b046a19b8598c78
Bastien Gili-tos <bast.gilitos@gmail.com>
2019-02-13 10:32:21 +0100

    Fix typo

--- server/src/services/hand/parse/src/utils/findFlushOrStraight.js
+++ server/src/services/hand/parse/src/utils/findFlushOrStraight.js
@@ -7,7 +7,7 @@
  * @param {Array} suits  Array of suits, e.g. ['S', 'D', 'C', 'S', 'S'].
  *
  * @returns {Object}     Object containing two properties. Its value property goes
- *                       from 0 for Highcard to 10 for Royal Flush. Its ordered property
+ *                       from 0 for Highcard to 9 for Royal Flush. Its ordered property
  *                       is an array of ordered card values for easy comparing in case of
  *                       a tie, for example [9, 9, 5, 5, 12] for 2 pairs.
  */
