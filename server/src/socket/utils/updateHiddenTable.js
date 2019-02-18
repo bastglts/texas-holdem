30073d7ca19ef1e5d87491fa4f9ea40aef4c6e6d
Bastien Gili-tos <bast.gilitos@gmail.com>
2019-02-18 13:10:59 +0100

    Change forEach to for loop

--- server/src/socket/utils/updateHiddenTable.js
+++ server/src/socket/utils/updateHiddenTable.js
@@ -3,16 +3,23 @@
 /* -------- Dependency ---------- */
 const Table = require('../../models/table');
 
+
 /**
  * Emit an `update_table` event and sends a table with hidden opponnent cards.
  *
  * @param {*} table Table object.
  * @param {*} io    Socket.Io instance.
  */
-module.exports = (table, io) => {
-  table.players.forEach(async player => {
+module.exports = async (table, io) => {
+  for (let i = 0; i < table.players.length; i++) {
+    // Current player
+    let player = table.players[i];
+
     // Retrieve fresh document from database
     const freshTable = await Table.findOne({ name: table.name });
+
+    // Hide deck of cards
+    freshTable.shuffledDeck = [];
 
     // Hide every opponents cards and hand
     freshTable.players.forEach(opponent => {
@@ -21,6 +28,7 @@ module.exports = (table, io) => {
         opponent.hand = {};
       }
     });
+
     io.to(`${player.ID}`).emit('update_table', freshTable);
-  });
+  }
 };
