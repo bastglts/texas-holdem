54142327329ec9df037f18baa53036261467b3d8
Bastien Gili-tos <bast.gilitos@gmail.com>
2019-02-22 18:48:44 +0100

    Add raise slider

--- client/src/components/TablePanel.vue
+++ client/src/components/TablePanel.vue
@@ -1,47 +1,55 @@
 <template>
   <transition name="fade" mode="out-in">
-    <p v-if="!table" class="table-panel" id="lw" :key="'load123'">
-      Loading...
-    </p>
+    <p v-if="!table" class="table-panel" id="lw" :key="'load123'">Loading...</p>
 
-    <p v-else-if="table.players.length === 1" class="table-panel" id="lw" :key="'wait123'">
-      You can't play alone... Please wait for at least one other player!
-    </p>
+    <p
+      v-else-if="table.players.length === 1"
+      class="table-panel"
+      id="lw"
+      :key="'wait123'"
+    >You can't play alone... Please wait for at least one other player!</p>
 
-    <p v-else-if="table.round === ''" class="table-panel" id="lw" :key="'wait456'">
-      Hand is about to start, waiting a few seconds for other players to join in...
-    </p>
+    <p
+      v-else-if="table.round === ''"
+      class="table-panel"
+      id="lw"
+      :key="'wait456'"
+    >Hand is about to start, waiting a few seconds for other players to join in...</p>
 
     <div v-else class="table-panel">
       <div class="poker-table" ref="pokertable">
-        <player v-for="(player, idx) in table.players"
-                :key="idx"
-                :id="`player${idx}`"
-                :pos="idx"
-                :player="player"/>
+        <player
+          v-for="(player, idx) in table.players"
+          :key="idx"
+          :id="`player${idx}`"
+          :pos="idx"
+          :player="player"
+        />
 
-        <p id="pot-count"> Pot: ${{table.pot}} </p>
+        <p id="pot-count">Pot: ${{table.pot}}</p>
 
-        <card class="board-card"
-              v-for="(card, idx) in table.board"
-              :key="card"
-              :id="`bc${idx}`"
-              :card="card"/>
+        <card
+          class="board-card"
+          v-for="(card, idx) in table.board"
+          :key="card"
+          :id="`bc${idx}`"
+          :card="card"
+        />
       </div>
 
       <div class="btm-table-panel">
         <template v-if="player.isSpeaking">
-          <button class="tbl-panel-btn" @click="action('fold')">Fold</button>
-          <button class="tbl-panel-btn" @click="action('call')">Call {{callAmount}} </button>
-          <button class="tbl-panel-btn" @click="action('raise')">Raise to {{raiseAmount}}</button>
+          <div class="tbl-panel-btns">
+            <button @click="action('fold')">Fold</button>
+            <button @click="action('call')">Call {{callAmount}}</button>
+            <button v-if="!allInCall" @click="action('raise')">Raise to {{raiseAmount}}</button>
+          </div>
 
-          <input type="text" v-model="raiseAmount" :placeholder="raiseAmount">
-<!--           <button class="table-panel-btn" @click="min">MIN</button>
-          <button class="table-panel-btn" @click="three">X3</button>
-          <button class="table-panel-btn" @click="four">X4</button>
-          <button class="table-panel-btn" @click="five">X5</button>
-          <button class="table-panel-btn" @click="fold">POT</button>
-          <button class="table-panel-btn" @click="fold">ALL IN</button> -->
+          <div v-if="!allInRaise" class="tbl-panel-inputs">
+            <input v-model="raiseAmount" type="range" :min="minRaise"
+              :max="player.count" class="slider">
+            <input type="text" v-model="raiseAmount" :placeholder="raiseAmount" id="raise-input">
+          </div>
         </template>
       </div>
     </div>
@@ -52,7 +60,6 @@
 <script>
 import Card from './Card.vue';
 import Player from './Player.vue';
-
 
 export default {
   name: 'TablePanel',
@@ -69,9 +76,13 @@ export default {
       table: undefined,
       player: undefined,
       raiseAmount: 0,
+      minRaise: 0,
       callAmount: 0,
+      allInCall: false,
+      allInRaise: false,
     };
   },
+
 
   methods: {
     action(act) {
@@ -84,12 +95,18 @@ export default {
     },
   },
 
+
   mounted() {
     this.socket.on('update_table', (table) => {
       this.table = table;
       this.player = table.players.find(plyr => plyr.username === this.user);
+
       this.callAmount = table.lastBet - this.player.lastBet;
-      this.raiseAmount = table.lastBet + (table.lastRaise || 20);
+      this.minRaise = table.lastBet + (table.lastRaise || table.bigBlind);
+      this.raiseAmount = this.minRaise;
+
+      this.allInCall = this.playerCount < this.callAmount;
+      this.allInRaise = this.playerCount < this.minRaise;
     });
   },
 
@@ -101,15 +118,26 @@ export default {
 
 
 <style>
-.tbl-panel-btn {
+.tbl-panel-btns,
+.tbl-panel-inputs {
+  height: 50%;
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+}
+
+.tbl-panel-btns button, #raise-input {
   padding: 0px;
-  width: 20%;
-  height: 40%;
+  height: 70%;
   font-size: 1.2vw;
   box-sizing: border-box;
   border-width: 1%;
   border-style: solid;
   border-color: #cf5c36;
+  opacity: 0.7;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
 }
 
 .poker-table {
@@ -122,22 +150,18 @@ export default {
 
 .btm-table-panel {
   display: flex;
+  flex-direction: column;
   background-color: #050517bd;
   box-shadow: 0px -5px 3px 2px #050517bd;
   height: 15%;
-  justify-content: center;
+  width: 100%;
 }
 
-.btm-table-panel input {
-  padding: 0px;
-  width: 20%;
-  height: 40%;
-  font-size: 1.2vw;
+#raise-input {
+  width: 10%;
+  height: 58%;
   color: white;
-  box-sizing: border-box;
-  border-width: 1%;
-  border-style: solid;
-  border-color: #cf5c36;
+  font-size: 1.1vw;
 }
 
 .table-panel {
@@ -150,12 +174,55 @@ export default {
   position: absolute;
   top: 32%;
   left: 50%;
-  transform: translate(-50%,0);
+  transform: translate(-50%, 0);
   color: #d6ac68;
 }
 
 #lw {
   margin: auto;
   font-size: 1.5vw;
+}
+
+.slider {
+  -webkit-appearance: none; /* Override default CSS styles */
+  appearance: none;
+  width: 100%;
+  height: 60%;
+  background: #cf5c36;
+  border: none;
+  outline: none;
+  padding: 0;
+  opacity: 0.7;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+}
+
+.slider:hover, .tbl-panel-btns button:hover, #raise-input:hover {
+  opacity: 1; /* Fully shown on mouse-over */
+}
+
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default CSS styles */
+  appearance: none;
+  height: 118%;
+  width: 6%;
+  padding: 4%;
+  border: none;
+  background-image: url('../assets/chip.svg');
+  background-size: cover;
+  background-color: #05051700;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  height: 118%;
+  width: 6%;
+  padding: 1%;
+  border: none;
+  background-image: url('../assets/chip.svg');
+  background-size: cover;
+  background-color: #05051700;
+  cursor: pointer;
 }
 </style>
